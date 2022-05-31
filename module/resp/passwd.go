c176d2517779e20dd8e88fcea4276d40adeaec94
zhanghao <lollipod@163.com>
2022-05-31 10:42:52 +0800

    fix:返回结构修改

--- module/resp/passwd.go
+++ module/resp/passwd.go
@@ -1,13 +1,17 @@
 package resp
 
 type PasswdInfosResp struct {
-	Id          uint64 `bson:"id"`          //id
-	UserId      int    `bson:"user_id"`     //用户
-	Username    string `bson:"user_name"`   //账户
-	Password    string `bson:"password"`    //密码
-	Description string `bson:"description"` //备注
-	Topic       string `bson:"topic"`       //标题
+	Id          uint64 `bson:"id " json:"id"`                  //id
+	UserId      int    `bson:"user_id" json:"userId"`          //用户
+	Username    string `bson:"user_name" json:"username"`      //账户
+	Password    string `bson:"password" json:"password"`       //密码
+	Description string `bson:"description" json:"description"` //备注
+	Topic       string `bson:"topic" json:"topic"`             //标题
 }
 type LoginResp struct {
 	Token string `json:"token"` //token
+}
+
+type Passwd struct {
+	Passwd []PasswdInfosResp `json:"passwds"`
 }
